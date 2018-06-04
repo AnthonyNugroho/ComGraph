2072e8e3a453e79fd664ab87f1dfab57d4b7db15
sen329 <hans.hilman@gmail.com>
2018-06-04 21:45:50 +0700

    adding core gameplay

--- initScene.js
+++ initScene.js
@@ -1,60 +1,168 @@
 var canvas, engine, scene, camera, ground;
-document.addEventListener("DOMContentLoaded",function(){
-//get renderCanvas
-canvas = document.getElementById("renderCanvas");
+document.addEventListener("DOMContentLoaded",function(){ onLoad();}, false);
 
-//create babylon engine
-engine = new BABYLON.Engine(canvas,true);
+var onLoad = function(){
+  //get renderCanvas
+  canvas = document.getElementById("renderCanvas");
 
-//create scene
-scene = new BABYLON.Scene(engine);
+  //create babylon engine
+  engine = new BABYLON.Engine(canvas,true);
 
+  initScene();
+  engine.runRenderLoop(function(){
+    if (! ship.killed) {
+          ship.move();
+          camera.position.z += ship.speed;
+          ship.position.z += ship.speed;
+          ground.position.z += ship.speed;
+      }
+      scene.render();
 
-gravity = new BABYLON.Vector3(0,-9.81,0);
-physicsEngine = new BABYLON.CannonJSPlugin();
+  });
+};
 
-scene.enablePhysics(gravity, physicsEngine);
+var initScene = function(){
+  scene = new BABYLON.Scene(engine);
 
-camera = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(0, 5, -30), scene);
-camera.maxZ = 1000;
-camera.speed = 4;
+  gravity = new BABYLON.Vector3(0,-9.81,0);
+  physicsEngine = new BABYLON.CannonJSPlugin();
 
+  scene.enablePhysics(gravity, physicsEngine);
 
-var mat = new BABYLON.StandardMaterial("material",scene);
-
-mat.specularColor = new BABYLON.Color3(0,1,0);
-mat.diffuseTexture = new BABYLON.Texture("ground.png",scene);
-//mat.specularTexture = new BABYLON.Texture("assets/grass.jpg",scene);
-mat.specularPower = 25;
-//mat.emissiveColor = new BABYLON.Color3(0.2,0.2,0.2);
-mat.ambientColor = new BABYLON.Color3(0.23,0.90,0.53);
-//transparency
-mat.alpha = 1;
-
-//create light
-var light = new BABYLON.HemisphericLight("hlight", new BABYLON.Vector3(0,8,0),scene);
-light.intensity = 0.6;
-
-var d = new BABYLON.DirectionalLight("dir", new BABYLON.Vector3(0,-0.5,0.5), scene);
-    d.position = new BABYLON.Vector3(0.1,100,-100);
-    d.intensity = 0.4;
-    // Purple haze, all around !
-    d.diffuse = BABYLON.Color3.FromInts(204,196,255);
-
-var ball = new BABYLON.Mesh.CreateSphere("ball", 10, 1, scene);
-  ball.position = new BABYLON.Vector3(0,10,10);
-
-ball.physicsImpostor = new BABYLON.PhysicsImpostor(ball, BABYLON.PhysicsImpostor.SphereImpostor, {mass:1, restitution:0.5, friction:0.2}, scene);
+  camera = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(0, 5, -30), scene);
+  camera.maxZ = 1000;
+  camera.speed = 4;
 
 
+  var mat = new BABYLON.StandardMaterial("material",scene);
 
-ground = new BABYLON.Mesh.CreateGround("ground", 800, 2000, 2, scene);
+  mat.specularColor = new BABYLON.Color3(0,1,0);
+  mat.diffuseTexture = new BABYLON.Texture("ground.png",scene);
+  //mat.specularTexture = new BABYLON.Texture("assets/grass.jpg",scene);
+  mat.specularPower = 25;
+  //mat.emissiveColor = new BABYLON.Color3(0.2,0.2,0.2);
+  mat.ambientColor = new BABYLON.Color3(0.23,0.90,0.53);
+  //transparency
+  mat.alpha = 1;
 
-ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor,{mass: 0, restitution: 0.2, friction: 0.2}, scene);
-ground.material = mat;
+  //create light
+  var light = new BABYLON.HemisphericLight("hlight", new BABYLON.Vector3(0,8,0),scene);
+  light.intensity = 0.6;
+
+  var d = new BABYLON.DirectionalLight("dir", new BABYLON.Vector3(0,-0.5,0.5), scene);
+      d.position = new BABYLON.Vector3(0.1,100,-100);
+      d.intensity = 0.4;
+      // Purple haze, all around !
+      d.diffuse = BABYLON.Color3.FromInts(204,196,255);
+
+      ship = new Ship(1, scene);
+
+  //var ball = new BABYLON.Mesh.CreateSphere("ball", 10, 1, scene);
+  //  ball.position = new BABYLON.Vector3(0,10,10);
+
+  //ball.physicsImpostor = new BABYLON.PhysicsImpostor(ball, BABYLON.PhysicsImpostor.SphereImpostor, {mass:1, restitution:0.5, friction:0.2}, scene);
 
 
-engine.runRenderLoop(function(){
-  scene.render();
-});
-});
+
+  ground = new BABYLON.Mesh.CreateGround("ground", 800, 2000, 2, scene);
+
+  ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor,{mass: 0, restitution: 0.2, friction: 0.2}, scene);
+  ground.material = mat;
+
+  scene.fogMode = BABYLON.Scene.FOGMODE_EXP2;
+   scene.fogDensity = 0.01;
+
+   setInterval(box, 100);
+   setInterval(bonus, 1000);
+}
+
+var box = function() {
+    var minZ = camera.position.z+500;
+    var maxZ = camera.position.z+1500;
+    var minX = camera.position.x - 100, maxX = camera.position.x+100;
+    var minSize = 2, maxSize = 10;
+
+    var randomX, randomZ, randomSize;
+
+    randomX = randomNumber(minX, maxX);
+    randomZ = randomNumber(minZ, maxZ);
+    randomSize = randomNumber(minSize, maxSize);
+
+    var b = BABYLON.Mesh.CreateBox("bb", randomSize, scene);
+
+    b.scaling.x = randomNumber(0.5, 1.5);
+    b.scaling.y = randomNumber(4, 8);
+    b.scaling.z = randomNumber(2, 3);
+
+    b.position.x = randomX;
+    b.position.y = b.scaling.y/2 ;
+    b.position.z = randomZ;
+
+    // action manager
+    b.actionManager = new BABYLON.ActionManager(scene);
+
+    // on collision with ship
+    var trigger = {trigger:BABYLON.ActionManager.OnIntersectionEnterTrigger, parameter: ship};
+    var exec = new BABYLON.SwitchBooleanAction(trigger, ship, "killed");
+    b.actionManager.registerAction(exec);
+
+    // on pick
+    // condition : ammo > 0
+    var condition = new BABYLON.ValueCondition(b.actionManager, ship, "ammo", 0, BABYLON.ValueCondition.IsGreater);
+
+    var onpickAction = new BABYLON.ExecuteCodeAction(
+        BABYLON.ActionManager.OnPickTrigger,
+        function(evt) {
+            if (evt.meshUnderPointer) {
+                var meshClicked = evt.meshUnderPointer;
+                meshClicked.dispose();
+                ship.ammo -= 1;
+                ship.sendEvent();
+            }
+        },
+        condition);
+
+    b.actionManager.registerAction(onpickAction);
+};
+
+var bonus = function() {
+    var bonus = BABYLON.Mesh.CreateSphere("sphere", 20, 5, scene);
+    bonus.material = new BABYLON.StandardMaterial("bonusMat", scene);
+    bonus.material.diffuseColor = BABYLON.Color3.Green();
+
+    var minZ = camera.position.z+500;
+    var maxZ = camera.position.z+1500;
+    var minX = camera.position.x - 100, maxX = camera.position.x+100;
+
+    bonus.position.x = randomNumber(minX, maxX);
+    bonus.position.y = 2.5;
+    bonus.position.z = randomNumber(minZ, maxZ);
+
+    bonus.actionManager = new BABYLON.ActionManager(scene);
+
+    // on collision with ship
+    var addAmmo = new BABYLON.IncrementValueAction(BABYLON.ActionManager.NothingTrigger, ship, "ammo", 1);
+    var destroyBonus = new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.NothingTrigger, function(evt) {
+        ship.sendEvent();
+        evt.source.dispose();
+    });
+
+    var trigger = {trigger:BABYLON.ActionManager.OnIntersectionEnterTrigger, parameter: ship};
+    var combine = new BABYLON.CombineAction(trigger, [addAmmo, destroyBonus]);
+    bonus.actionManager.registerAction(combine);
+
+};
+
+/**
+ * Random number
+ * @param min
+ * @param max
+ * @returns {number}
+ */
+var randomNumber = function (min, max) {
+    if (min == max) {
+        return (min);
+    }
+    var random = Math.random();
+    return ((random * (max - min)) + min);
+};
